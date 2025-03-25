a451e44e553e1aec00a7503577253077736ccb36
bilboJB <computersjavier@gmail.com>
2025-03-24 20:56:36 -0400

    cambio

--- utils/validate.ts
+++ utils/validate.ts
@@ -1,4 +1,4 @@
-export function validEmail(email : string | null) {
+export const validEmail = (email : string | null) => {
     if (!email) return false
     return /\S+@\S+\.\S+/.test(email)
-}
+};

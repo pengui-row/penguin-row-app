5d729da1e6897f92768fa91bb13a1a5fe246adaf
bilboJB <computersjavier@gmail.com>
2025-04-08 12:35:50 -0400

    paginas de registro

--- utils/validate.ts
+++ utils/validate.ts
@@ -3,4 +3,8 @@ export class Validation {
         if (!email) return false
         return /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/.test(email)
     };
+    validPhone = (phone : string | null) => {
+        if (!phone) return false
+        return /^[0-9]{10}$/.test(phone)
+    };
 }

b92dea1ada7bd20e60d796f6f65278f0a2f2186d
raphaell22 <Rafaelcarmona295@gmail.com>
2025-05-03 20:22:26 -0400

    Integre la funcionalidad de inicio de sesión con la API

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,14 +3,16 @@ import { Stack } from "expo-router";
 import { StatusBar } from 'expo-status-bar';
 import { useColorScheme } from "@/hooks/useColorScheme";
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-
+import { AuthProvider } from "./context/AuthContext";
 
 export default function RootLayout() {
-    const colorScheme = useColorScheme();
-    return (
-        <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-            <Stack screenOptions={{ headerShown: false }} />
-            <StatusBar style="auto" />
-        </ThemeProvider>
-    );
+  const colorScheme = useColorScheme();
+  return (
+    <AuthProvider>
+      <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+        <Stack screenOptions={{ headerShown: false }} />
+        <StatusBar style="auto" />
+      </ThemeProvider>
+    </AuthProvider>
+  );
 }
